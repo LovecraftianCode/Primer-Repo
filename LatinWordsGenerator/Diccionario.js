dfbf47a81217e8f3aa83d01b9bdc6036064935ad
CraftianCode <humbertop_c-3@hotmail.com>
2026-02-28 02:08:37 -0600

    Se crea la funcion repeatLatinPhrase con la que en un futuro se creearan los nombres de habilidades de un videojuego

--- LatinWordsGenerator/Diccionario.js
+++ LatinWordsGenerator/Diccionario.js
@@ -1,8 +1,13 @@
+//Pagina fuente de palabras en latin: https://web.archive.org/web/20071219155115/http://users.adelphia.net/~florusc/latine/latinePage14.html
+
 let dic =["abbatia", "acardiotrophia", "abbreviatio", "acatalepsia", "abbreviatio","abductio","Abellinensis","abiatio","abiogensis","Aboa",
         "abortus","abstractio","abusive","academia","acatalepticus","accelerare","acceleratio","acceleratorium","acceptus","accordeon","accusator",
         "acephalus","acetonium","achne","automatum","autonomia","autoplaustrum", "autoraeda laophorica", "avis Canaria", "avolare", "avolatio", "azimutum",
         "azoicus", "azotum", "Aztecus", "Babel", "babelicus", "baccalaureatus", "baccalaureus", "bacchanalia", "bacilliformis", "bacillum nicotianum", "bacterium Salmonenis",
-        "badiolus", "bahaismus", "bahaistes", "bakelitis","ballatio", "ballipedium", "balneae pensiles"
+        "badiolus", "bahaismus", "bahaistes", "bakelitis","ballatio", "ballipedium", "balneae pensiles","balneatrix","balneotherapia","brevio",
+        "bronchitis", "bronchopneumonia","bryologia", "Buddhismus", "Buddhista", "bulgula nummaria","bulimus","buphthalmia", "burculum","butyrificium", 
+        "cabbalista", "caccabus", "cacophonia", "cactaceae", "cactus", "cadaverum", "caeliscalpium", "caeneathesis", "caenozoicus", "cafaeina", "cafaeopola", 
+        "cafaeum", "calceamenta cummea", "calceamentarium", "calchaemia", "caleidoscopium", "calligraphia", "calomelanum"
     ]
 
 function generateLatinWord() {
@@ -18,6 +23,17 @@ function generateLatinphrase(wordCount) {
     return phrase.trim();
 }
 
+function repeatLatinPhrase(phrase, times) {
+    let phrases = [];
+    let repeatedPhrase = generateLatinWord();
+    for (let i = 0; i < times; i++) {
+        phrases.push(`${phrase} ${repeatedPhrase}`);
+        repeatedPhrase = generateLatinWord();
+    }
+    return phrases;
+}
 
 let latinPhrase = generateLatinphrase(2);
 console.log("Generated Latin phrase: " + latinPhrase);
+
+console.log(repeatLatinPhrase(generateLatinWord(), 10));
